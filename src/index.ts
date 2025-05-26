604ddab79205e657c2960b713e7e9da424fde894
Jempasam <samueldemont83@gmail.com>
2025-05-26 14:29:10 +0200

    as

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./WamGUIGenerator.ts";
 export * from "./control/Control.ts";
 export * from "./control/ControlMap.ts";
 export * from "./control/controls.ts";
+export * from "./control/ControlStateManager.ts";
