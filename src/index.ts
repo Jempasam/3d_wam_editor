4c390255580551659ac91c4f146a935bff9b2913
Jempasam <samueldemont83@gmail.com>
2025-06-05 20:44:21 +0200

    Example list available in API exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from "./control/Control.ts";
 export * from "./control/ControlMap.ts";
 export * from "./control/controls.ts";
 export * from "./control/ControlStateManager.ts";
+export {default as examples} from "./examples.json";
