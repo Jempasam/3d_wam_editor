df6c5671411af37a850e41cab57287a815b79d63
Jempasam <samueldemont83@gmail.com>
2025-07-08 14:00:43 +0200

    update web

--- update_web.sh
+++ update_web.sh
@@ -1,7 +1,10 @@
 git checkout web
 git merge main -m "Merge main into web branch"
 npm run build
+sleep 1
 npm add -A
+sleep 1
 npm commit -m "Update web build"
+sleep 1
 npm push
 git checkout main
